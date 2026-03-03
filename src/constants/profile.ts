f3d2e6f0d767b6439de3ae0d49749c940e214ebf
stacy <edc2312321@gmail.com>
2026-03-03 09:52:17 +0800

    typo: email

--- src/constants/profile.ts
+++ src/constants/profile.ts
@@ -2,7 +2,7 @@ export const PROFILE = {
   name: 'HSIN YU CHENG',
   role: 'Frontend Engineer',
   siteName: "Stacy's Portfolio",
-  email: 'hsinyu.c.2000@gmali.com',
+  email: 'hsinyu.c.2000@gmail.com',
   github: 'https://github.com/XINYU714',
   summary: '擁有 3 年以上經驗的前端工程師，專注 Vue 生態系，擅長將複雜需求轉化為高效、可維護的 Web 體驗。',
 }
