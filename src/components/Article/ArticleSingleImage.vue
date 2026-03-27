3e7129ab9deb37864f901ea416a0245b303c1483
stacy <edc2312321@gmail.com>
2026-03-27 16:22:02 +0800

    feat: 單圖元件 - VusUse:useImage

--- src/components/Article/ArticleSingleImage.vue
+++ src/components/Article/ArticleSingleImage.vue
@@ -1,11 +1,14 @@
 <script lang="ts" setup>
+import { useImage } from '@vueuse/core'
 import type { ArticleImageItem } from '@/types'
 
-defineProps<ArticleImageItem>()
+const props = defineProps<ArticleImageItem>()
+const { isLoading } = useImage({ src: props.path })
 </script>
 <template>
 	<figure class="my-5">
-			<img :src="path" :alt="alt" class="w-full h-full object-cover rounded-lg">
-			<figcaption v-if="content" class="text-center text-sm text-muted mt-2">{{ content }}</figcaption>
+		<div v-if="isLoading" class="w-full h-60 rounded-lg bg-secondary animate-pulse"></div>
+		<img v-else :src="path" :alt="alt" class="w-full h-full object-cover rounded-lg">
+		<figcaption v-show="content" class="text-center text-sm text-muted mt-2">{{ content }}</figcaption>
 	</figure>
 </template>
--- src/components/ExperienceCard.vue
+++ src/components/ExperienceCard.vue
@@ -13,7 +13,6 @@ defineProps<ExperienceItem>()
         <!-- 各個區塊 -->
         <div v-for="(section, index) in description.sections" :key="index">
           <h4 class="text-lg font-bold text-accent mb-2">{{ section.title }}</h4>
-          
           <div v-for="(item, i) in section.items" :key="i" class="ml-1.5 px-3 pb-4 last:pb-0 border-l border-accent">
             <h5 class="text-body font-bold text-contrast mb-1">{{ item.title }}</h5>
             <p class="text-sm text-muted leading-relaxed" v-html="item.content"></p>
@@ -21,7 +20,7 @@ defineProps<ExperienceItem>()
         </div>
       </div>
       <div class="flex flex-wrap gap-2 mt-5">
-        <div v-for="(item, index) in skills" :key="index" class="tag">{{ item }}</div>
+        <div v-for="(item) in skills" :key="item" class="tag">{{ item }}</div>
       </div>
     </div>
   </div>
